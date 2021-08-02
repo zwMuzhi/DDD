f5d4e6c87b5b6a54eeb4aaac1a883e8f1f00ae70
zwMuZhi <45135559+zwMuzhi@users.noreply.github.com>
2021-08-02 21:26:01 +0800

    zwMuzhi
    项目初始化

---
+++ src/test/java/cn/muzhi/ddd/DddApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.muzhi.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
